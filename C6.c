271b2c29005aded42098e698389efa3ceb089368
harshmeet0015 <harshmeet842@gmail.com>
2026-08-17 14:08:06 +0530

    Create C6.c

---
+++ C6.c
@@ -0,0 +1,35 @@
+**day 6**
+  (1)
+#include<stdio.h>
+int main()
+{
+int num1;
+printf("enter your number");
+scanf("%d \n ",&num1);
+if(num1%2==0)
+printf(" your choice is EVEN");
+else 
+printf(" ODD");
+return 0;
+}
+**(2)**
+  #include<stdio.h>
+int main()
+{
+int  num;
+printf("enter your integer:");
+scanf("%d",&num);
+if(num>=0)
+{
+printf("the number is positive ");
+}
+if(num==0)
+{
+printf(" the numbe ris zero");
+}
+else 
+{
+printf(" the  numbe ris negative ");
+}
+return 0;
+}
