10e32232e2318e5cd4eaa4e23a6459d0611076fd
harshmeet0015 <harshmeet842@gmail.com>
2026-09-22 13:37:08 +0530

    Add string analysis and modification program

---
+++ replace.c
@@ -0,0 +1,58 @@
+**day 44**
+(1)
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    char str[200];
+    int i, spaces = 0, digits = 0, special = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == ' ')
+        {
+            spaces++;
+        }
+        else if (isdigit(str[i]))
+        {
+            digits++;
+        }
+        else if (!isalpha(str[i]) && str[i] != '\n')
+        {
+            special++;
+        }
+    }
+
+    printf("Spaces = %d\n", spaces);
+    printf("Digits = %d\n", digits);
+    printf("Special characters = %d\n", special);
+
+    return 0;
+}
+2
+#include <stdio.h>
+
+int main()
+{
+    char str[200];
+    int i;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == ' ')
+        {
+            str[i] = '-';
+        }
+    }
+
+    printf("Modified string: %s", str);
+
+    return 0;
+}
